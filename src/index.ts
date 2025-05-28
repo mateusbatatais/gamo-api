acb27036e25a50eea1475fc1770b86054f97749e
Mateus Severiano <mateusbatatais@gmail.com>
2025-05-28 15:07:42 -0300

    mudar cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
