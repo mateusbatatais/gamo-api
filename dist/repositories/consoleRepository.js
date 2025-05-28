5806003f5ae6c3616046a372ceec394290badff0
Mateus Severiano <mateusbatatais@gmail.com>
2025-05-28 15:02:45 -0300

    remove a dist do git

--- dist/repositories/consoleRepository.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.findVariantBySlug = void 0;
-const db_1 = require("../lib/db");
-const findVariantBySlug = (consoleId, slug) => db_1.db.consoleVariant.findUnique({
-    where: { consoleId_slug: { consoleId, slug } },
-});
-exports.findVariantBySlug = findVariantBySlug;
