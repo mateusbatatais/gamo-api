bc8fd80f4b8565bbe1cd97288d1cd10500af1cfd
Mateus Severiano <mateusbatatais@gmail.com>
2025-06-04 14:20:57 -0300

    passa pra true

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
