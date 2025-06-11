8b0d9407a678affab6f53c01085ce56f0fc3e603
mateusbatatais <mateusbatatais@gmail.com>
2025-06-11 07:53:13 -0300

    corrige build

--- src/routes/consoles.ts
+++ src/routes/consoles.ts
@@ -1,10 +1,15 @@
-//src/routes/consoles.ts
-
+// src/routes/consoles.ts
 import { RequestHandler, Router } from "express";
 import { listConsoleVariantsHandler } from "../controllers/consoleController";
 import { validate } from "../middleware/validate";
 import { listConsoleVariantsSchema } from "../validators/listConsoleVariants";
 
 const router = Router();
-router.get("/", validate(listConsoleVariantsSchema), listConsoleVariantsHandler as RequestHandler);
+
+router.get(
+  "/",
+  validate(listConsoleVariantsSchema),
+  listConsoleVariantsHandler as unknown as RequestHandler,
+);
+
 export default router;
