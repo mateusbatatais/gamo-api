d769c88fb609d7e41f1a89d462e7da9d902b0160
mateusbatatais <mateusbatatais@gmail.com>
2025-06-11 07:30:10 -0300

    Ajuste de marca

--- src/controllers/brandController.ts
+++ src/controllers/brandController.ts
@@ -2,7 +2,7 @@
 import { RequestHandler } from "express";
 import { getBrands } from "../services/brandService";
 import { BrandDTO } from "../dtos/brand.dto";
-import { ListBrandsDTO } from "../validators/listBrands";
+import { ListBrandsDTO, listBrandsSchema } from "../validators/listBrands"; // Usando o esquema de validação
 
 export const listBrandsHandler: RequestHandler<object, BrandDTO[], object, ListBrandsDTO> = async (
   req,
@@ -10,6 +10,9 @@ export const listBrandsHandler: RequestHandler<object, BrandDTO[], object, ListB
   next,
 ) => {
   try {
+    // Aqui você deve aplicar a validação no corpo da requisição
+    listBrandsSchema.parse(req); // Valida os parâmetros da requisição usando Zod
+
     const brands: BrandDTO[] = await getBrands();
     res.json(brands);
   } catch (err) {
