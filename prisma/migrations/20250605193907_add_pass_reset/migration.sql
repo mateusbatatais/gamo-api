662951c22f2669115dd70e744d79e058dc8f343d
mateusbatatais <mateusbatatais@gmail.com>
2025-06-20 22:52:46 -0300

    unifica migrations

--- prisma/migrations/20250605193907_add_pass_reset/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "passwordResetToken" TEXT,
-ADD COLUMN     "passwordResetTokenExpires" TIMESTAMP(3);
--- prisma/migrations/20250614004015_add_gen/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Console" ALTER COLUMN "releaseDate" SET DATA TYPE DATE;
-
--- AlterTable
-ALTER TABLE "Skin" ALTER COLUMN "releaseDate" SET DATA TYPE DATE;
